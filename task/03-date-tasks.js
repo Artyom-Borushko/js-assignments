2e9342eac25f575299ece78f1d346688253de27d
Artyom Borushko <artyom.borushko@gmail.com>
2020-07-07 18:53:43 +0300

    add 03-date-tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
 }
 
 
@@ -76,7 +77,19 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let start = startDate.getTime();
+   let end = endDate.getTime();
+   let span = new Date(end - start);
+   let hours = Math.floor(span.getTime() / 3600000);
+   let minutes = span.getMinutes();
+   let seconds = span.getSeconds();
+   let milliseconds = span.getMilliseconds();
+   hours = hours < 10 ? '0' + hours : hours;
+   minutes = minutes < 10 ? '0' + minutes : minutes;
+   seconds = seconds < 10 ? '0' + seconds : seconds;
+   milliseconds = milliseconds < 10 ? '00' + milliseconds :
+      milliseconds < 100 ? '0' + milliseconds : milliseconds;
+   return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
@@ -94,7 +107,8 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   let angle = Math.abs(0.5 * (60 * (date.getUTCHours() % 12) - 11 * (date.getUTCMinutes() % 60)));
+   return (angle <= 180 ? angle : 360 - angle) * Math.PI / 180;
 }
 
 
